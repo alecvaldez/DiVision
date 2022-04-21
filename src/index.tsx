f9c141f5f27666fb4410be37577ae1207406de38
alecvaldez <a1valdez@ucsd.edu>
2022-04-21 11:18:55 -0700

    Added firebase to project

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCngDQpA2XIehOHzgtIUF8YbNQrJbAHCbU",
+  authDomain: "division-91b2e.firebaseapp.com",
+  projectId: "division-91b2e",
+  storageBucket: "division-91b2e.appspot.com",
+  messagingSenderId: "650721899687",
+  appId: "1:650721899687:web:12d97613bc9373df49d782",
+  measurementId: "G-SPDFXT0MWY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
